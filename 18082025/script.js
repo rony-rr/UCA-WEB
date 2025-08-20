641de76b9131b92ec4b2d2bd34a2aab7a1ede1a4
Rony Santos <rony.sant20@gmail.com>
2025-08-20 13:58:48 -0600

    add feature

---
+++ 18082025/script.js
@@ -0,0 +1,17 @@
+// script.js
+console.log("Hello!");
+
+function greet(name) {
+  alert("Hello, " + name + "!");
+}
+
+
+/**
+ * FutureNav - Main JavaScript
+ * script.js - Interactive functionality for the futuristic navigation experience
+ * March 24, 2025
+ */
+
+document.addEventListener('DOMContentLoaded', () => {
+    
+})
