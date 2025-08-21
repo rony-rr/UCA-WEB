adae65e48be3bccf8a2cfb5cd48a9bc8d21aec26
Rony Santos <rony.sant20@gmail.com>
2025-08-21 15:50:03 -0600

    feature: add Examples

--- script.js
+++ script.js
@@ -1,35 +1,18 @@
 // script.js
 
 document.addEventListener("DOMContentLoaded", () => {
-  const elementH3 = document.querySelector("#content > h3");
-  const elementWrap = document.querySelector("#wrap");
+  const elementP = document.querySelector("#content > p");
+  const elementWrap = document.querySelector("#content > #wrap__imgs");
 
-  function changeColorh3() {
-    elementH3.style.color = "white";
+  function changeTextP() {
+    elementP.innerHTML = "Esta funcion se ejecuto.";
   }
 
-  function wrapChangeColor() {
-    elementWrap.style.backgroundColor = "#F6339A";
+  function wrapImgsChangeOrder() {
+    elementWrap.style.flexDirection = "column";
   }
-
-  function redirect() {
-    window.location.assign("https://www.w3schools.com/tags/default.asp");
-  }
-
-  function changeTexth3() {
-    elementH3.innerHTML = "Nuevo Título";
-  }
-
-  setTimeout(() => {
-    elementH3.innerHTML = "Cambio automático del texto";
-  }, 5000);
-
-  // 👇 asocia los botones aquí mejor que con "onclick" en HTML
+  
   document.querySelector(".wrapButtons button:nth-child(1)").onclick =
-    wrapChangeColor;
-  document.querySelector(".wrapButtons button:nth-child(2)").onclick = redirect;
-  document.querySelector(".wrapButtons button:nth-child(3)").onclick =
-    changeTexth3;
-  document.querySelector(".wrapButtons button:nth-child(4)").onclick =
-    changeColorh3;
+    changeTextP;
+  document.querySelector(".wrapButtons button:nth-child(2)").onclick = wrapImgsChangeOrder;
 });
