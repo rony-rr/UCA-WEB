af7ea8639b0d4e29228566b673e8967b6e4060a7
Rony Santos <rony.sant20@gmail.com>
2025-08-26 22:35:10 -0600

    feature: add example fetch

--- script.js
+++ script.js
@@ -1,18 +1,49 @@
 // script.js
 
+// inicializador de contenido JS hasta tener cargado el DOM
 document.addEventListener("DOMContentLoaded", () => {
-  const elementP = document.querySelector("#content > p");
-  const elementWrap = document.querySelector("#content > #wrap__imgs");
+  const elementP = document.querySelector("p#texto__variable");
+  const loadingElement = document.querySelector("p#texto__carga");
 
-  function changeTextP() {
-    elementP.innerHTML = "Esta funcion se ejecuto.";
+  // función de cambio de texto desde un API externa
+  // usamos una función asincronica
+  async function chargeText() {
+    loadingElement.style.display = "block";
+
+    // Acá el Fetch API devuelve un objeto Promise
+    await fetch("https://jsonplaceholder.typicode.com/users/1")
+      .then((respuesta) => respuesta.json())
+      .then((datos) => {
+        setTimeout(() => {
+          changeTextP(`Nombre: ${datos.name}`);
+          loadingElement.style.display = "none";
+        }, 1000);
+      });
   }
 
-  function wrapImgsChangeOrder() {
-    elementWrap.style.flexDirection = "column";
+  // uso mediante la ejecución de async/await
+  async function chargeTextAwait() {
+    // Dado que Fetch devuelve un objeto Promise,
+    // esto significa que también puede usar la sintaxis async/await
+    // para reemplazar los métodos .then() y .catch()
+    // otra forma de escrkibirlo seía mediante un cambio de manejo
+    loadingElement.style.display = "block";
+    const respuesta = await fetch(
+      "https://jsonplaceholder.typicode.com/users/2"
+    );
+    const jsonRes = await respuesta.json();
+    setTimeout(() => {
+      changeTextP(`Email: ${jsonRes.email}`);
+      loadingElement.style.display = "none";
+    }, 1000);
   }
-  
+
+  function changeTextP(name) {
+    elementP.textContent = name;
+  }
+
   document.querySelector(".wrapButtons button:nth-child(1)").onclick =
-    changeTextP;
-  document.querySelector(".wrapButtons button:nth-child(2)").onclick = wrapImgsChangeOrder;
+    chargeText;
+  document.querySelector(".wrapButtons button:nth-child(2)").onclick =
+    chargeTextAwait;
 });
