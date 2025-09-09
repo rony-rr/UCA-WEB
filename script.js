aa8fafa31a406d9a2b95d20c4719b08a3ca2b0d1
Rony Santos <rony.sant20@gmail.com>
2025-09-09 15:08:44 -0600

    add example

--- script.js
+++ script.js
@@ -2,42 +2,79 @@
 
 // inicializador de contenido JS hasta tener cargado el DOM
 document.addEventListener("DOMContentLoaded", async () => {
-  const arrTpmPosts = [];
+  const arrMenus = [];
 
-  const recoverPosts = () => {
-    const content = `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum`;
+  const CreateMenus = () => {
+    let content = `
+      It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+    `;
 
-    for(let i=0; i < 50; i++) {
+    for (let i = 0; i < 50; i++) {
       let obj = {
         id: i + 1,
-        content
+        content,
       };
 
-      arrTpmPosts.push(obj);
+      arrMenus.push(obj);
     }
   };
 
-  recoverPosts();
+  CreateMenus();
 
-  const FnAddPost = () => {
-    let posts = '';
-    arrTpmPosts.forEach((el, i) => {
-      const post = `
-        <div class="post">
-          <h4 id"title_promo_${el.id}" class="title_promo">Promocion: ${el.id}</h4>
-          <p class="promo_content">${el.content}</p>
+  const AddMenus = () => {
+    let menus = "";
+    arrMenus.forEach((el, i) => {
+      const menu = `
+        <div class="menu">
+          <h4 id"title_menu_${el.id}" class="title_menu">Menu: ${el.id}</h4>
+          <p class="menu_content">${el.content}</p>
         </div>
       `;
 
-      posts = `${posts}${post}`;
+      menus = `${menus}${menu}`;
     });
 
-    document.getElementById("content").innerHTML = posts;
-
-    console.log({posts}); 
+    document.getElementById("content").innerHTML = menus;
   };
 
-  FnAddPost();
+  AddMenus();
+
+  // const arrTpmPosts = [];
+
+  // const recoverPosts = () => {
+  //   const content = `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum`;
+
+  //   for(let i=0; i < 50; i++) {
+  //     let obj = {
+  //       id: i + 1,
+  //       content
+  //     };
+
+  //     arrTpmPosts.push(obj);
+  //   }
+  // };
+
+  // recoverPosts();
+
+  // const FnAddPost = () => {
+  //   let posts = '';
+  //   arrTpmPosts.forEach((el, i) => {
+  //     const post = `
+  //       <div class="post">
+  //         <h4 id"title_promo_${el.id}" class="title_promo">Promocion: ${el.id}</h4>
+  //         <p class="promo_content">${el.content}</p>
+  //       </div>
+  //     `;
+
+  //     posts = `${posts}${post}`;
+  //   });
+
+  //   document.getElementById("content").innerHTML = posts;
+
+  //   console.log({posts});
+  // };
+
+  // FnAddPost();
 
   // const post = '<div class="post"></div>';
   // document.getElementById("content").innerHTML = post;
